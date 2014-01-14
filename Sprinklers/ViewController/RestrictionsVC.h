d84f519c1496fb0f9dca933423d1d3d9e6e22711
Adrian Manolache <adrian.manolachecg@gmail.com>
2014-01-14 12:14:24 +0200

    - added restrictions view controller

---
+++ Sprinklers/ViewController/RestrictionsVC.h
@@ -0,0 +1,13 @@
+//
+//  RestrictionsVC.h
+//  Sprinklers
+//
+//  Created by Adrian Manolache on 07/01/14.
+//  Copyright (c) 2014 Tremend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RestrictionsVC : UIViewController
+
+@end
