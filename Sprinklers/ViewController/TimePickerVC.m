0754aed5af1165cc5f1b60bc3c5300af45fc7217
Istvan Sipos <istvan@Istvans-MacBook-Pro.local>
2014-11-18 15:01:07 +0200

    Fixed the time picker for 24-hour-cycle format

--- Sprinklers/ViewController/TimePickerVC.m
+++ Sprinklers/ViewController/TimePickerVC.m
@@ -72,7 +72,6 @@
         }
     }
     else {
-        _timeFormat = 1;
         if ([[UIDevice currentDevice] iOSGreaterThan:7]) {
             self.leftConstraint.constant = 110;
             if (_timeFormat == 0) // 24 hours
