f4548e2162f0cb2774d8e2b6cfce3b621e01afab
matyas.fabian <fabianmatyas@gmail.com>
2014-11-25 20:55:45 +0200

    Re-put "Add Cloud Account" button to Cloud accounts screen

--- Sprinklers/CloudAccountsVC.m
+++ Sprinklers/CloudAccountsVC.m
@@ -8,6 +8,10 @@
 
 #import "CloudAccountsVC.h"
 #import "CloudEmailDevicesVC.h"
+#import "AddNewCell.h"
+#import "Constants.h"
+#import "+UILabel.h"
+#import "AddNewDeviceVC.h"
 
 @interface CloudAccountsVC ()
 
@@ -34,6 +38,8 @@
     for (NSDictionary *cloudD in sprinklersByEmail) {
         self.cloudResponsePerEmails[cloudD[@"email"]] = cloudD;
     }
+    [self.tableView registerNib:[UINib nibWithNibName:@"AddNewCell" bundle:nil] forCellReuseIdentifier:@"AddNewCell"];
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -57,44 +63,56 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     // Return the number of sections.
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return self.cloudEmails.count;
+    if (section == 0) {
+        return self.cloudEmails.count;
+    }
+    
+    return 1;
 }
 
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    UITableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:@"Debug"];
-    if (!cell) {
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Debug"];
+    if (indexPath.section == 0) {
+        UITableViewCell *cell =  [tableView dequeueReusableCellWithIdentifier:@"Debug"];
+        if (!cell) {
+            cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Debug"];
+        }
+        
+        cell.textLabel.text = self.cloudEmails[indexPath.row];
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+        NSDictionary *details = self.cloudResponsePerEmails[cell.textLabel.text];
+        cell.detailTextLabel.text = [NSString stringWithFormat:@"activeCount:%d knownCount:%d authCount:%d",
+                                     [details[@"activeCount"] intValue],
+                                     [details[@"knownCount"] intValue],
+                                     [details[@"authCount"] intValue]];
+        cell.detailTextLabel.textColor = [UIColor darkGrayColor];
+        
+        return cell;
     }
+
+    // Add Cloud Account
+    AddNewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AddNewCell" forIndexPath:indexPath];
+    cell.selectionStyle = (self.tableView.isEditing ? UITableViewCellSelectionStyleNone : UITableViewCellSelectionStyleGray);
+    [cell.plusLabel setCustomRMFontWithCode:icon_Add size:24];
     
-    cell.textLabel.text = self.cloudEmails[indexPath.row];
-    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-    NSDictionary *details = self.cloudResponsePerEmails[cell.textLabel.text];
-    cell.detailTextLabel.text = [NSString stringWithFormat:@"activeCount:%d knownCount:%d authCount:%d",
-                                 [details[@"activeCount"] intValue],
-                                 [details[@"knownCount"] intValue],
-                                 [details[@"authCount"] intValue]];
-    cell.detailTextLabel.textColor = [UIColor darkGrayColor];
+    [cell.plusLabel setTextColor:[UIColor colorWithRed:kWateringGreenButtonColor[0] green:kWateringGreenButtonColor[1] blue:kWateringGreenButtonColor[2] alpha:1]];
+    [cell.titleLabel setTextColor:[UIColor colorWithRed:kWateringGreenButtonColor[0] green:kWateringGreenButtonColor[1] blue:kWateringGreenButtonColor[2] alpha:1]];
     
+    cell.titleLabel.text = @"Add cloud account";
+
     return cell;
 }
 
-
-/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return indexPath.section == 0;
 }
-*/
-
 
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -127,16 +145,22 @@
 
 // In a xib-based application, navigation from a table can be handled in -tableView:didSelectRowAtIndexPath:
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Navigation logic may go here, for example:
-    // Create the next view controller.
-    CloudEmailDevicesVC *detailViewController = [[CloudEmailDevicesVC alloc] init];
-    NSString *email = self.cloudEmails[indexPath.row];
-    detailViewController.devices = self.cloudSprinklers[email];
-    detailViewController.email = email;
-    // Pass the selected object to the new view controller.
-    
-    // Push the view controller.
-    [self.navigationController pushViewController:detailViewController animated:YES];
+    if (indexPath.section == 0) {
+        // Navigation logic may go here, for example:
+        // Create the next view controller.
+        CloudEmailDevicesVC *detailViewController = [[CloudEmailDevicesVC alloc] init];
+        NSString *email = self.cloudEmails[indexPath.row];
+        detailViewController.devices = self.cloudSprinklers[email];
+        detailViewController.email = email;
+        // Pass the selected object to the new view controller.
+        
+        // Push the view controller.
+        [self.navigationController pushViewController:detailViewController animated:YES];
+    } else {
+        AddNewDeviceVC *addNewDeviceVC = [[AddNewDeviceVC alloc] init];
+        addNewDeviceVC.cloudUI = YES;
+        [self.navigationController pushViewController:addNewDeviceVC animated:YES];
+    }
 }
 
 
