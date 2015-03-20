f8c55b104ade047cdc279a3322f81a7cdd888275
Istvan Sipos <istvan@Istvans-MacBook-Pro.local>
2015-03-20 13:24:59 +0200

    Fixed #208 Please update your RainMachine to version (null)

--- Sprinklers/Managers/UpdateManager.m
+++ Sprinklers/Managers/UpdateManager.m
@@ -204,7 +204,7 @@
             newVersion = updateInfo.the_new_version;
         } else {
             for (UpdateInfo4PackageDetails *packageDetails in updateInfo4.packageDetails) {
-                if ([packageDetails.packageName isEqualToString:@"rainmachine"]) {
+                if ([packageDetails.packageName isEqualToString:@"rainmachine-app"]) {
                     newVersion = packageDetails.theNewVersion;
                     break;
                 }
