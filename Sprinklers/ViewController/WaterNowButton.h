6956110b977f815668903baad3b98bdfae7fc594
Istvan Sipos <istvan@192-168-0-103.rdsnet.ro>
2015-05-13 13:56:02 +0300

    Fixed #288 Water Now - circle buttons present tearing on the sides and upper and lower part

---
+++ Sprinklers/ViewController/WaterNowButton.h
@@ -0,0 +1,13 @@
+//
+//  WaterNowButton.h
+//  Sprinklers
+//
+//  Created by Istvan Sipos on 13/05/15.
+//  Copyright (c) 2015 Tremend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WaterNowButton : UIButton
+
+@end
